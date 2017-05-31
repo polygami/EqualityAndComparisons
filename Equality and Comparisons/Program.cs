8ff052146792fb9dd3a0182333dab2edb52f12d4
polygami <liam@polygami.co.uk>
2017-05-31 10:50:51 +0200

    Add project files.

---
+++ Equality and Comparisons/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equality_and_Comparisons
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
